7f4d99ec96fe25097d5aff7cbe0bdc06ef9712c3
K-ash-ish <kashishsondhiya@gmail.com>
2022-01-29 16:37:44 +0530

    added footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { createNav, createhome } from "./home";
 
 
 const contentPage = document.querySelector("#content");
+const footer = document.createElement("footer");
 contentPage.append(createNav())
 contentPage.append(createhome());
+
+const footerEl = `
+        <p>Made With ❤️ <a href="https://github.com/K-ash-ish">Kashish</a></p>
+    `;
+footer.insertAdjacentHTML("afterbegin",footerEl)
+contentPage.append(footer)
 
